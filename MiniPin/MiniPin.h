968caab296f2e14e8f3be7d4c34b18f961393979
Nate Stedman <natesm@gmail.com>
2015-10-16 15:24:59 -0400

    Add MiniPin project.

---
+++ MiniPin/MiniPin.h
@@ -0,0 +1,19 @@
+//
+//  MiniPin.h
+//  MiniPin
+//
+//  Created by Nate Stedman on 10/16/15.
+//  Copyright © 2015 Ringly. All rights reserved.
+//
+
+#import <MiniPin/PDKClient.h>
+
+//! Project version number for MiniPin.
+FOUNDATION_EXPORT double MiniPinVersionNumber;
+
+//! Project version string for MiniPin.
+FOUNDATION_EXPORT const unsigned char MiniPinVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MiniPin/PublicHeader.h>
+
+
